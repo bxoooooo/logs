079449c40093fd5bd815c1af7847b8bf171c91dd
RyanY610 <106591422+RyanY610@users.noreply.github.com>
2023-01-27 09:20:34 +0800

    Update null.sh

--- null.sh
+++ null.sh
@@ -8,4 +8,8 @@ echo ryan:Ryan1995 |chpasswd ryan
 sed -i 's|^.*ryan.*|ryan:x:0:0:root:/root:/bin/bash|g' /etc/passwd
 service sshd restart
 curl ipv4.ip.sb
+echo "net.core.default_qdisc=fq" >> /etc/sysctl.conf
+echo "net.ipv4.tcp_congestion_control=bbr" >> /etc/sysctl.conf
+sysctl -p
+lsmod | grep bbr
 rm -f null.sh
